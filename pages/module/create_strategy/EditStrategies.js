52915294cc6d3c2888f0eb45f3c976585ac8ce8f
Ashsenior <ashutoshjunior@gmail.com>
2022-11-16 02:00:23 -0300

    Fg

--- pages/module/create_strategy/EditStrategies.js
+++ pages/module/create_strategy/EditStrategies.js
@@ -3,6 +3,7 @@ import { Box } from '@mui/system';
 import React from 'react';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { ShoppingCart } from '@mui/icons-material';
+import CreateMeetingModal from '../../dashboards/Meetings/CreateMeetingModal';
 
 const applications = [
     {
@@ -44,16 +45,65 @@ const applications = [
 const EditStrategies = () => {
     return (
         <>
-        <div className='w-full bg-white'>
-                <div className='w-11/12 flex items-center justify-between mx-auto px-4 rounded-lg py-3'>
-                    <h1 className='text-xl font-semibold'>Submit💡?</h1>
-                    <button
-                        onClick={() => Router.push('/create_startup')}
-                        type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Create New Startup
-                    </button>
+        <div className='w-full bg-indigo-500 border-b rounded-b-xl '>
+            <div className='w-12/12 flex items-center justify-between mx-auto px-2 rounded-lg py-2'>
+                <h1 className='text-xl text-white font-semibold'>Did the strategy worked?</h1>
+                <label
+                    // onClick={() => Router.push('/create_startup')}
+                    htmlFor="edit-startups-modal"
+                    type="button" className="inline-flex items-center px-2 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-900 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Submit the results
+                </label>
+            </div>
+        </div>
+
+        <input type="checkbox" id="edit-startups-modal" className="modal-toggle" />
+        <div className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box bg-white">
+                <div className="modal-action flex flex-col">
+                    <h1 className='text-xl font-semibold my-4 text-center m-2'>Submit and close this strategy</h1>
+                    <Box
+                        className='mt-4'
+                        sx={{
+                            maxWidth: '100%',
+                        }}
+                    >
+                        <TextField
+                                style={{ width: "100%" }}
+                                id="outlined-multiline-static"
+                                label="Strategy outcomes"
+                                multiline
+                                minRows={4}
+                            />
+                </Box>
+                    <Box
+                        className='mt-4'
+                    >
+                        <TextField label="Metrics met" id="fullWidth" />
+                    </Box>
+                    <div class="max-w-xl m-4">
+                        <label
+                            class="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+                            <span class="flex items-center space-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                                </svg>
+                                <span class="font-medium text-gray-600">
+                                    Drop result documents , or 
+                                    <span class="text-blue-600 underline mx-1">browse</span>
+                                </span>
+                            </span>
+                            <input type="file" name="file_upload" class="hidden" />
+                        </label>
+                    </div>
+                    <label htmlFor="edit-startups-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label type="button" htmlFor="edit-startups-modal" className="btn mt-2 border-0 bg-indigo-500 text-white">Submit and Close Strategy</label>
                 </div>
             </div>
+        </div>
+
         <div className='mt-6'>
             <div className='flex items-center justify-between'>
                 <h1 className='text-xl font-semibold'>Strategy Name (Major)</h1>
