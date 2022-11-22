c10519360491cbe51377cfceae3a004d46ea61e8
Shubham Joshi <83740922+shubhamjoshi99899@users.noreply.github.com>
2022-11-22 19:45:54 +0530

    mapped api data with components

--- pages/module/create_strategy/EditStrategies.js
+++ pages/module/create_strategy/EditStrategies.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { ShoppingCart } from '@mui/icons-material';
 import CreateMeetingModal from '../../dashboards/Meetings/CreateMeetingModal';
+import { useState } from 'react';
 
 const applications = [
     {
@@ -42,7 +43,8 @@ const applications = [
     },
 ]
 
-const EditStrategies = () => {
+const EditStrategies = ({strategy}) => {
+
     return (
         <>
         <div className='w-full bg-indigo-500 border-b rounded-b-xl '>
@@ -106,18 +108,18 @@ const EditStrategies = () => {
 
         <div className='mt-6'>
             <div className='flex items-center justify-between'>
-                <h1 className='text-xl font-semibold'>Strategy Name (Major)</h1>
+                <h1 className='text-xl font-semibold'>Strategy Name {strategy?.strategyTitle}</h1>
                 <Link to="" type='button' className='btn btn-sm'>In Progress</Link>
             </div>
             <medium>Strategy leader 
                 <p className="px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                @ashsenior
+                {strategy?.strategyLeader}
                 </p></medium>
-            <h4>Start Date: <span>11/12/2022</span></h4>
+            <h4>Start Date: <span>{strategy?.approxStartDate}</span></h4>
             <div className='grid grid-cols-1 xl:grid-cols-2 p-2' >
                 <div className='mt-6'>
                     <h4 className='text-lg font-semibold'>Customer it impacts</h4>
-                    <p className='p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, mollitia est nihil laudantium explicabo itaque omnis impedit fugiat officiis, voluptas, aut saepe voluptates odio praesentium recusandae velit? Debitis dignissimos nesciunt eos qui! Blanditiis nisi id esse omnis, consectetur modi nulla sit sunt! Reiciendis illo explicabo laboriosam voluptates consequuntur vel quas corporis.</p>
+                    <p className='p-1'>{strategy?.customer}</p>
                 </div>
                 <div className='mt-2'>
                     <h4 className='text-base font-semibold'>Success Metrics</h4>
