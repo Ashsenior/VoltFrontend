529883535b6667679f3772cebc5edd2b74ff9eb4
Shubham Joshi <83740922+shubhamjoshi99899@users.noreply.github.com>
2022-11-22 19:17:26 +0530

    mapped api data with components

--- pages/dashboards/Idea/IdeaCard.js
+++ pages/dashboards/Idea/IdeaCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const IdeaCard = () => {
+const IdeaCard = ({startup}) => {
     return (
         <div className='mr-0 md:mr-4'>
             <div className="card card-compact w-full stats rounded-lg stats-vertical lg:stats-horizontal shadow-lg">
                 <div className="card-body">
                     <h4 className='text-xl text-gray-700 font-semibold'>Idea 💡</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta hic consequatur commodi quaerat aperiam deserunt. Inventore saepe quis pariatur sed earum natus, ullam voluptas, minus quas quisquam cum tempore odit?</p>
+                    <p>{startup?.idea}</p>
                     <div className="card-actions justify-between border-t-2">
                         <h4 className=' text-blue-500 font-semibold mt-1'>Idea Phase</h4>
 
