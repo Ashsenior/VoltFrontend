d2f1a3b144d65d10230eeaf2c355304ee36b9940
shubhamjoshi99899 <shubhamjoshi99899@gmail.com>
2022-11-24 09:57:15 +0530

    binded home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -8,6 +8,7 @@ import { get } from '../../config/axiosClient';
 import { useEffect } from 'react';
 import { Button, Container } from '@mui/material';
 import axiosInstance from "../../src/axiosAPi";
+import PublicStartupCard from './PublicStartupCard';
 
 const Home = () => {
     const [your_startups, setYourStartups] = useState([]);
@@ -28,8 +29,10 @@ const Home = () => {
               params: { username: localStorage.getItem("username") },
             })
             .then((response) => {
-              if (response.status == 200) {
+              if (response?.status == 200) {
                 console.log(response);
+                setYourStartups(response.data?.your_startups);
+                setAllStartups(response.data?.all_startups)
               }
             });
         } catch (error) {
@@ -63,7 +66,6 @@ const Home = () => {
             {/* Startup cards */}
             <div className='grid grid-cols-1 md:grid-cols-1 xl:grid-cols-1'>
                 <StartupCard startup={your_startups} />
-                <StartupCard />
             </div>
             {/* All Startups */}
             <div className='w-11/12 mx-3 my-2 mt-6 flex items-center justify-start gap-2'>
@@ -72,10 +74,8 @@ const Home = () => {
             </div>
             {/* All startup Cards */}
             <div className='grid grid-cols-1 md:grid-cols-1 xl:grid-cols-1'>
-                <StartupCard />
-                <StartupCard />
-                <StartupCard />
-                <StartupCard />
+                <PublicStartupCard all_startup={all_startups} />
+                
             </div>
         </div>
     );
